feaae6b92936c9010b167e7834b03f35152ec1a3
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-11-08 00:26:02 -0500

    Warehouse header for mobile

--- src/Components/pages/Warehouse/Warehouse.js
+++ src/Components/pages/Warehouse/Warehouse.js
@@ -7,16 +7,16 @@ const Warehouse = () => {
     return (
         <div className='warehouse'>
             <div className='warehouse__header'>
-                <h1 className="warehouse__header-title">Warehouse</h1>
-                <div className='warehouse__header-search'>
-                    <input className="warehouse__header-search__left" type="text" id="searchInput" placeholder="Search"></input>
-                    <img className="warehouse__header-search__loupe" src={search} alt="small loupe for search"></img>
+                <h1 className="warehouse__header-title">Warehouses</h1>
+                <div className="warehouse__header-search">
+                    <input className="warehouse__header-search__text" type="text" id="searchInput" placeholder="Search..." />
+                    <img className="warehouse__header-search__icon" src={search} alt="Search" />
                 </div>
-                <Link to='#'>
+                {/* <Link to='#'> */}
                     <button className="warehouse__header-button">
                         <p className="warehouse__header-button__text">+ Add New Warehouse</p>
                     </button>
-                </Link>
+                {/* </Link> */}
             </div>
         </div>
     );
