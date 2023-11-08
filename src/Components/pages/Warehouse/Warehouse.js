d724e3cf03c5f587c1f495b6030140c539aa94e1
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-11-07 23:25:47 -0500

    Warehouse Inventory List Component

--- src/Components/pages/Warehouse/Warehouse.js
+++ src/Components/pages/Warehouse/Warehouse.js
@@ -1,5 +1,25 @@
+import './Warehouse.scss';
+import '../../../App.scss';
+import { Link } from 'react-router-dom';
+import search from '../../../assets/Icons/search-24px.svg';
+
 const Warehouse = () => {
-    return ("placeholder");
+    return (
+        <div className='warehouse'>
+            <div className='warehouse__header'>
+                <h1 className="warehouse__header-title">Warehouse</h1>
+                <div className='warehouse__header-search'>
+                    <input className="warehouse__header-search__left" type="text" id="searchInput" placeholder="Search"></input>
+                    <img className="warehouse__header-search__loupe" src={search} alt="small loupe for search"></img>
+                </div>
+                <Link to='#'>
+                    <button className="warehouse__header-button">
+                        <p className="warehouse__header-button__text">+ Add New Warehouse</p>
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
 }
- 
+
 export default Warehouse;
