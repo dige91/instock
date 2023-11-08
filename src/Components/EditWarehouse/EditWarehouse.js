9dab8947599171c8cdc618edbe14de65a333c592
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-11-08 17:28:35 -0500

    Warehouse tablet styling

---
+++ src/Components/EditWarehouse/EditWarehouse.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import './EditWarehouse.scss';
+
+function EditWarehouse() {
+    return (
+        <></>
+    )
+}
+
+export default EditWarehouse;
--- src/components/WarehouseItem/WarehouseItem.js
+++ src/components/WarehouseItem/WarehouseItem.js
@@ -36,10 +36,10 @@ const WarehouseItem = ({ warehouse }) => {
 
                 <img className="item__right-update" src={edit} alt="edit icon"></img>
             </div>
-            {/* <div className="item__icons">
-                <img className="item__icons-delete" src={del} alt="delete icon"></img>
-                <img className="item__icons-update" src={edit} alt="edit icon"></img>
-            </div> */}
+            <div className="item__icons2">
+                <img className="item__icons2-delete" src={del} alt="delete icon"></img>
+                <img className="item__icons2-update" src={edit} alt="edit icon"></img>
+            </div>
         </div>
         </>
     )
