0f08a53651a7ddf84e8222d43d0afcbb0848441e
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-11-08 01:07:20 -0500

    Warehouse header completed

--- src/Components/pages/Warehouse/Warehouse.js
+++ src/Components/pages/Warehouse/Warehouse.js
@@ -5,21 +5,23 @@ import search from '../../../assets/Icons/search-24px.svg';
 
 const Warehouse = () => {
     return (
-        <div className='warehouse'>
-            <div className='warehouse__header'>
-                <h1 className="warehouse__header-title">Warehouses</h1>
-                <div className="warehouse__header-search">
-                    <input className="warehouse__header-search__text" type="text" id="searchInput" placeholder="Search..." />
-                    <img className="warehouse__header-search__icon" src={search} alt="Search" />
+
+        <div className="warehouses">
+            <div className="warehouses__header">
+                <h1 className="warehouses__header-title">Warehouses</h1>
+                <div className="warehouses__header-container">
+                    <form className="warehouses__header-form">
+                        <input type="search" name="search" className="warehouses__header-form-search" placeholder="Search..." />
+                        <Link to="#">
+                            <button className="warehouses__header-form-button">+ Add New Warehouse</button>
+                        </Link>
+                    </form>
                 </div>
-                {/* <Link to='#'> */}
-                    <button className="warehouse__header-button">
-                        <p className="warehouse__header-button__text">+ Add New Warehouse</p>
-                    </button>
-                {/* </Link> */}
             </div>
         </div>
+
     );
 }
 
 export default Warehouse;
+
