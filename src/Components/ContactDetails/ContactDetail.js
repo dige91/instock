da5947838ee063acef4d5c50e84542a227543b1b
Kevin Aristhomene <swoledevkev@gmail.com>
2023-11-08 22:50:00 -0500

    completed the front end for the add warehouse component

--- src/Components/ContactDetails/ContactDetail.js
+++ src/Components/ContactDetails/ContactDetail.js
@@ -3,18 +3,18 @@ import './ContactDetails.scss'
 function ContactDetails (){
     return (
             <div className='contact'>
-                <h2>Contact Details</h2>
-                <label className='contact__label' htmlFor='name'>Contact Name</label>
-                <input className='contact__input' placeholder='Contact Name' type='text' id='name' name='name'></input>
+                <h2 className='contact__heading'>Contact Details</h2>
+                <label className='contact__label' htmlFor='contact_name'>Contact Name</label>
+                <input className='contact__input' placeholder='Contact Name' type='text' id='contact_name' name='contact_name'></input>
 
-                <label className='contact__label' htmlFor='position'>Position</label>
-                <input className='contact__input' placeholder='Position' type='text' id='position' name='position'></input>
+                <label className='contact__label' htmlFor='contact_position'>Position</label>
+                <input className='contact__input' placeholder='Position' type='text' id='contact_position' name='contact_position'></input>
 
-                <label className='contact__label' htmlFor='number' >Phone Number</label>
-                <input className='contact__input' placeholder='Phone Number' type='tel' id='number'></input>
+                <label className='contact__label' htmlFor='contact_phone' >Phone Number</label>
+                <input className='contact__input' placeholder='Phone Number' type='tel' id='contact_phone' name='contact_phone'></input>
 
-                <label className='contact__label' htmlFor='email' >Email</label>
-                <input className='contact__input' placeholder='Email' type='email' id='email'></input>
+                <label className='contact__label' htmlFor='contact_email' >Email</label>
+                <input className='contact__input' placeholder='Email' type='contact_email' id='contact_email'></input>
                 
             </div>
     )
