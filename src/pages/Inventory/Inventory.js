267ca04cab2a49cffdcdff9620f4c810850e58e7
Daniel Di Geronimo <dige91@gmail.com>
2023-11-11 15:05:12 -0500

    adusting the merge inventory.js

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -4,18 +4,12 @@ import DeleteInventory from "../../components/DeleteInventory/DeleteInventory";
 import './Inventory.scss';
 import WarehouseCard from '../../components/WarehouseCard/WarehouseCard';
 
-<<<<<<< HEAD
-const Inventory = ( {warehouses , setWarehouses} )=> {
-    return (
-        <WarehouseCard  warehouses={warehouses} setWarehouses={setWarehouses} />
-=======
 const Inventory = ()=> {
     return (
     <>
     <DeleteInventory/>
                  Loading ...
     </>
->>>>>>> develop
     );
 }
 
